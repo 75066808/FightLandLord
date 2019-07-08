398f2ed8e988ae3971362badf6e6dcd7f1dd9f9b
ForsakeLJK <forsakeljk@gmail.com>
2019-07-08 23:42:26 +0800

    now a single card can be showed, but still cannot figure out how to set its coordinates arbitrarily.

--- code/FightLandLord/FightLandLord/window.h
+++ code/FightLandLord/FightLandLord/window.h
@@ -3,6 +3,18 @@
 #define WINDOW_H
 #include <QtWidgets/QMainWindow>
 #include "ui_window.h"
+#include <vector>
+#include <QImage>
+#include <QMessageBox>
+#include <QPainter>
+#include <QRect>
+#include <QPoint>
+#include <QSize>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
 
 class Window : public QMainWindow
 {
@@ -13,7 +25,12 @@ public:
 
 private:
 	Ui::FightLandLordClass* ui;
-
+	std::vector<QImage> cards; 
+	// QPixmap* cardTempPixmap ;
+	// QGraphicsPixmapItem* cardTempPixmapItem;
+	// QGraphicsScene* Scene;
+public slots:
+	void showCardsInHand();
 };
 
 #endif
