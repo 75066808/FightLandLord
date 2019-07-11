5d8fcd31aa28459f3ae946fb5f65218613e7b109
YuRuiJing <75066808@qq.com>
2019-07-11 23:25:29 +0800

    window module update

--- code/FightLandLord/FightLandLord/window/window/window.cpp
+++ code/FightLandLord/FightLandLord/window/window/window.cpp
@@ -8,67 +8,18 @@ Window::Window(QWidget *parent)
 	
 	scene.setSceneRect(0, 0, 1, 1);
 
-	colorName[0] = "spade";
-	colorName[1] = "heart";
-	colorName[2] = "club";
-	colorName[3] = "diamond";
+	initAll();
 
-	for (qint8 value = 3;value < 18;value++)
-	{
-		if (value >= 3 && value <= 10)
-			valueName[value] = QString::number(value);
-		else if (value == 11)
-			valueName[value] = "J";
-		else if (value == 12)
-			valueName[value] = "Q";
-		else if (value == 13)
-			valueName[value] = "K";
-		else if (value == 14)
-			valueName[value] = "A";
-		else if (value == 15)
-			valueName[value] = "2";
-		else if (value == 16)
-			valueName[value] = "joker1";
-		else
-			valueName[value] = "joker2";
-	}
+	connect(&button[ENTER_BTN], SIGNAL(clicked()), this, SLOT(connectButtonClick()));
+	connect(&button[READY_BTN], SIGNAL(clicked()), this, SLOT(readyButtonClick()));
 
-	QPixmap pixmap;
-	for (qint32 color = 0;color < 4;color++)
-	{
-		for (qint32 value = 3;value <= 15;value++)
-		{
-			QString path;
-			path.append("poker_resource/");
-			path.append(colorName[color]);
-			path.append(valueName[value]);
-			pixmap.load(path, "jpg");
-			initItem(pixmap, cardItem[color][value], 0.1, 0.15);
-		}
-	}
-
-	pixmap.load("poker_resource/joker1", "jpg");
-	initItem(pixmap, cardItem[0][16], 0.1, 0.15);
-
-	pixmap.load("poker_resource/joker2", "jpg");
-	initItem(pixmap, cardItem[0][17], 0.1, 0.15);
-
-	addParentItemToScene(cardSlot, 0.2, 0.6, 0.6, 0.2);
+	addButtonToScene(button[ENTER_BTN], 0.4, BTN_TOP);
 	
-	pixmap.load("button_resource/connect", "jpg");
-	initButton(pixmap, enterButton, 0.2, 0.1);
-	addButtonToScene(enterButton, 0.2, 0.4);
-
-	pixmap.load("button_resource/ready", "jpg");
-	initButton(pixmap, readyButton, 0.2, 0.1);
-	addButtonToScene(readyButton, 0.6, 0.4);
-	
-	
-	connect(&enterButton, SIGNAL(clicked()), this, SLOT(connectButtonClick()));
-	connect(&readyButton, SIGNAL(clicked()), this, SLOT(readyButtonClick()));
+	addParentItemToScene(cardSlot, SLOT_LEFT, SLOT_TOP, SLOT_WIDTH, SLOT_HEIGHT);
 
 	ui.graphicsView->setScene(&scene);
 	ui.graphicsView->show();
+
 }
 
 
@@ -77,28 +28,21 @@ void Window::windowNotificationSlot(std::shared_ptr<Signal> signal)
 {
 	qDebug() << "View Model to Window";
 
-	qreal x = 0.2;
-	qreal y = 0.1;
+	clearScreen();
+	addParentItemToScene(cardSlot, SLOT_LEFT, SLOT_TOP, SLOT_WIDTH, SLOT_HEIGHT);
 
 	switch (signal->signalType)
 	{
 	case CONNECT_SUCCESS:
 		qDebug() << "Connect success";
+		addButtonToScene(button[READY_BTN], 0.4, BTN_TOP);
 		break;
 	case READY:
 		qDebug() << "Ready";
+		addItemToScene(stateItem[READY_ITEM][0], 0.4, BTN_TOP);
 		break;
 	case DEAL_CARD:
-		for (qint32 i = 0; i < *onHandNum;i++)
-		{
-			qint32 color = onHandCard->cards[i].color;
-			qint32 value = onHandCard->cards[i].i;
-			addItemToParentItem(cardItem[color][value], cardSlot, x, y);
-			x += 0.03;
-		}
-
-		ui.graphicsView->setScene(&scene);
-		ui.graphicsView->show();
+		drawSelfCard();
 		break;
 	default:
 		return;
@@ -121,22 +65,126 @@ void Window::resizeEvent(QResizeEvent* size)
 }
 
 
-void Window::initItem(QPixmap pixmap, QGraphicsPixmapItem &item, qreal rw, qreal rh)
+
+void Window::initAll(void)
 {
-	item.setPixmap(pixmap.scaled(rw*width(), rh*height()));
+	QString colorName[4];
+	QString valueName[16];
+	QString tempName("JQKA2");
+
+	colorName[0] = "spade";
+	colorName[1] = "heart";
+	colorName[2] = "club";
+	colorName[3] = "diamond";
+
+	for (qint8 value = 3;value < 16;value++)
+	{
+		if (value >= 3 && value <= 10)
+			valueName[value] = QString::number(value);
+		else
+			valueName[value] = tempName[value - 11];
+	}
+
+	for (qint32 color = 0;color < 4;color++)
+	{
+		for (qint32 value = 3;value <= 15;value++)
+		{
+			QString path;
+			path.append("Resources/poker/");
+			path.append(colorName[color]);
+			path.append(valueName[value]);
+			path.append(".jpg");
+			initItem(cardItem[color][value], path, CARD_WIDTH, CARD_HEIGHT);
+		}
+	}
+
+	QString path;
+	path = "Resources/poker/joker1.jpg";
+	initItem(cardItem[0][16], path, CARD_WIDTH, CARD_HEIGHT);
+	path = "Resources/poker/joker2.jpg";
+	initItem(cardItem[0][17], path, CARD_WIDTH, CARD_HEIGHT);
+
+
+	initItem(stateItem[READY_ITEM][0], "Resources/state/readyState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[READY_ITEM][1], "Resources/state/readyState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[READY_ITEM][2], "Resources/state/readyState.jpg", STATE_WIDTH, STATE_HEIGHT);
+
+	initItem(stateItem[SKIP_LL_ITEM][0], "Resources/state/skipLLState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[SKIP_LL_ITEM][1], "Resources/state/skipLLState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[SKIP_LL_ITEM][2], "Resources/state/skipLLState.jpg", STATE_WIDTH, STATE_HEIGHT);
+
+	initItem(stateItem[SKIP_CARD_ITEM][0], "Resources/state/skipCardState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[SKIP_CARD_ITEM][1], "Resources/state/skipCardState.jpg", STATE_WIDTH, STATE_HEIGHT);
+	initItem(stateItem[SKIP_CARD_ITEM][2], "Resources/state/skipCardState.jpg", STATE_WIDTH, STATE_HEIGHT);
+
+
+	initButton(button[ENTER_BTN], "border-image: url(:/Button/Resources/button/connect.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[READY_BTN], "border-image: url(:/Button/Resources/button/ready.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[QUIT_BTN], "border-image: url(:/Button/Resources/button/disconnect.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[CHOOSE_LL_BTN], "border-image: url(:/Button/Resources/button/chooseLL.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[SKIP_LL_BTN], "border-image: url(:/Button/Resources/button/skipLL.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[PLAY_CARD_BTN], "border-image: url(:/Button/Resources/button/playCard.jpg);", BTN_WIDTH, BTN_HEIGHT);
+	initButton(button[SKIP_CARD_BTN], "border-image: url(:/Button/Resources/button/skipCard.jpg);", BTN_WIDTH, BTN_HEIGHT);
+
+
 }
 
-void Window::initButton(QPixmap pixmap, QPushButton &button, qreal rw, qreal rh)
+void Window::drawState(void)
 {
-	QIcon icon(pixmap.scaled(rw*width(), rh*height()));
-	button.setIcon(icon);
+	//switch()
+}
+
+void Window::drawSelfCard(void)
+{
+	qreal top = ON_HAND_TOP;
+	qreal left = 0.5 - ((*onHandNum - 1) * ON_HAND_INT + CARD_WIDTH / SLOT_WIDTH) / 2;
+
+	for (qint32 i = 0; i < *onHandNum;i++)
+	{
+		qint32 color = onHandCard->cards[i].color;
+		qint32 value = onHandCard->cards[i].i;
+		addItemToParentItem(cardItem[color][value], cardSlot, left, top);
+		left += ON_HAND_INT;
+	}
+}
+
+void Window::drawSelfPlayCard(void)
+{
+
+}
+void Window::drawUpperPlayCard(void)
+{
+
+}
+void Window::drawLowerPlayCard(void)
+{
+
+}
+void Window::drawBackGround(void)
+{
+
+}
+
+
+void Window::clearScreen(void)
+{
+	foreach(QGraphicsItem *item, scene.items())
+	{
+		scene.removeItem(item);
+	}
+}
+
+
+
+void Window::initItem(QGraphicsPixmapItem &item, QString path, qreal rw, qreal rh)
+{
+	item.setPixmap(QPixmap(path).scaled(rw*width(), rh*height()));
+}
+
+void Window::initButton(QPushButton &button, QString path, qreal rw, qreal rh)
+{
+	button.setStyleSheet(path);
 	button.setGeometry(0, 0, rw*width(), rh*height());
-}
-
-void Window::addParentItemToScene(QGraphicsRectItem &parent, qreal rx, qreal ry, qreal rw, qreal rh)
-{
-	parent.setRect(QRectF(rx*width(), ry*height(), rw*width(), rh*height()));
-	scene.addItem(&parent);
 }
 
 void Window::addItemToScene(QGraphicsPixmapItem &item, qreal rx, qreal ry)
@@ -145,26 +193,37 @@ void Window::addItemToScene(QGraphicsPixmapItem &item, qreal rx, qreal ry)
 	scene.addItem(&item);
 }
 
+void Window::addParentItemToScene(QGraphicsRectItem &parent, qreal rx, qreal ry, qreal rw, qreal rh)
+{
+	parent.setRect(QRectF(rx*width(), ry*height(), rw*width(), rh*height()));
+	scene.addItem(&parent);
+}
+
 void Window::addItemToParentItem(QGraphicsPixmapItem &item, QGraphicsRectItem &parent, qreal rx, qreal ry)
 {
-	item.setPos(parent.rect().left() + rx*parent.rect().width(), parent.rect().top() + ry*parent.rect().height());
+	item.setPos(parent.rect().left() + rx * parent.rect().width(), parent.rect().top() + ry * parent.rect().height());
 	item.setParentItem(&parent);
 }
 
 void Window::addButtonToScene(QPushButton &button, qreal rx, qreal ry)
 {
-	button.setGeometry(rx*width(), ry*height(), button.width(),button.height());
+	button.setGeometry(rx*width(), ry*height(), button.width(), button.height());
 	scene.addWidget(&button);
 }
+
+
 
 
 void Window::connectButtonClick(void)
 {
 	qDebug() << "Connect button Click";
+
 	std::shared_ptr<Signal> signal = std::make_shared<Signal>();
 
 	signal->signalType = CONNECT;
 	emit windowCommandSignal(signal);
+
+	button[0].setFocus();
 }
 
 void Window::readyButtonClick(void)
@@ -179,7 +238,6 @@ void Window::readyButtonClick(void)
 
 void customScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-	/*
 	QTransform transform;
 	QGraphicsItem *item;
 
@@ -193,6 +251,7 @@ void customScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 	//qreal original_x = (qreal) item->pos().x()+1;
 	//qreal original_y = (qreal) item->pos().y()+1;
 
+	
 	if (item && item->parentItem())
 	{
 		int index = item->parentItem()->childItems().indexOf(item);
--- code/FightLandLord/FightLandLord/window/window/window.h
+++ code/FightLandLord/FightLandLord/window/window/window.h
@@ -1,15 +1,6 @@
 #pragma once
-#ifndef WINDOW_H
-#define WINDOW_H
 #include <QtWidgets/QMainWindow>
 #include "ui_window.h"
-#include <vector>
-#include <QImage>
-#include <QMessageBox>
-#include <QPainter>
-#include <QRect>
-#include <QPoint>
-#include <QSize>
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QGraphicsItem>
@@ -18,8 +9,57 @@
 #include <QGraphicsSceneMouseEvent>
 #include <QTransform>
 #include <QPushButton>
-#include <QtAlgorithms>
+
 #include "../utility/common/common.h"
+
+#define ENTER_BTN         0
+#define QUIT_BTN          1
+#define READY_BTN         2
+#define CHOOSE_LL_BTN     3
+#define SKIP_LL_BTN       4
+#define PLAY_CARD_BTN     5
+#define SKIP_CARD_BTN     6
+  
+#define READY_ITEM        0
+#define SKIP_LL_ITEM      1
+#define SKIP_CARD_ITEM    2
+
+#define BTN_WIDTH         0.2
+#define BTN_HEIGHT        0.08
+
+#define STATE_WIDTH       0.2
+#define STATE_HEIGHT      0.1
+
+#define CARD_WIDTH        0.15
+#define CARD_HEIGHT       0.2
+
+#define BTN_TOP           0.5
+#define BTN_INT           0.05
+
+#define SLOT_LEFT         0.1
+#define SLOT_TOP          0.6
+#define SLOT_WIDTH        0.8
+#define SLOT_HEIGHT       0.35
+
+#define ON_HAND_TOP       0.3
+#define ON_HAND_INT       0.05 
+
+#define SELF_PLAY_TOP     0.4
+#define SELF_PLAY_INT     0.03
+
+#define UPPER_PLAY_LEFT   0.2
+#define UPPER_PLAY_TOP    0.3
+#define UPPER_PLAY_INT    0.03
+
+#define LOWER_PLAY_RIGHT  0.8
+#define LOWER_PLAY_TOP    0.3
+#define LOWER_PLAY_INT    0.03
+
+#define UPPER_STATE_LEFT  0.2
+#define UPPER_STATE_TOP   0.3
+
+#define LOWER_STATE_RIGHT 0.8
+#define LOWER_STATE_TOP   0.3
 
 class customScene : public QGraphicsScene
 {
@@ -30,7 +70,6 @@ class Window : public QMainWindow
 {
 	Q_OBJECT
 
-	friend class customScene;
 public:
 	Window(QWidget *parent = Q_NULLPTR);
 
@@ -66,22 +105,32 @@ private:
 
 	customScene scene;
 	QGraphicsPixmapItem cardItem[4][18];
+	QGraphicsPixmapItem stateItem[3][3];
+	QPushButton button[7];
 	QGraphicsRectItem cardSlot;
-	QPushButton enterButton;
-	QPushButton readyButton;
 
-	void initItem(QPixmap pixmap, QGraphicsPixmapItem &item, qreal rw, qreal rh);
-	void initButton(QPixmap pixmap, QPushButton &button, qreal rw, qreal rh);
+	void initAll(void);
 
-	void addParentItemToScene(QGraphicsRectItem &parent, qreal rx, qreal ry, qreal rw, qreal rh);
+	void drawState(void);
+	void drawSelfCard(void);
+
+	void drawSelfPlayCard(void);
+	void drawUpperPlayCard(void);
+	void drawLowerPlayCard(void);
+
+	void drawBackGround(void);
+
+	void clearScreen(void);
+
+
+	void initItem(QGraphicsPixmapItem &item, QString path, qreal rw, qreal rh);
+	void initButton(QPushButton &button, QString path, qreal rw, qreal rh);
+
 	void addItemToScene(QGraphicsPixmapItem &item, qreal rx, qreal ry);
+	void addParentItemToScene(QGraphicsRectItem &parent, qreal rx, qreal ry, qreal rw, qreal rh);
 	void addItemToParentItem(QGraphicsPixmapItem &item, QGraphicsRectItem &parent, qreal rx, qreal ry);
-
+	
 	void addButtonToScene(QPushButton &button, qreal rx, qreal ry);
-
-
-	QString colorName[4];
-	QString valueName[18];
 
 signals:
 	void windowCommandSignal(std::shared_ptr<Signal> signal);
@@ -92,5 +141,3 @@ public slots:
 	void windowNotificationSlot(std::shared_ptr<Signal> signal);
 
 };
-
-#endif
